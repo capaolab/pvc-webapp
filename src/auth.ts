fb6c6525e7885e1a0deba72e90790eb7a1852e90
viriatonova <viriatonova@gmail.com>
2025-07-27 13:08:20 -0300

    feat: revert to auth on front and use external api

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,6 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 
-import { handleAuthentication } from './lib/auth.lib';
 import { signInSchema } from './lib/zod.schemas';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
@@ -12,18 +11,12 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: {},
       },
       authorize: async credentials => {
-        let user = null;
-
+        const user = null;
         const { email, password } = await signInSchema.parseAsync(credentials);
-
-        // logic to verify if the user exists
-        // user = await handleAuthentication(email, password)
-        user = 'viriato';
 
         if (!user) {
           throw new Error('Invalid credentials.');
         }
-
         return user;
       },
     }),
